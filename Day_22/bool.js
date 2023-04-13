90f3ee095bd4a107acd98a6977d17f17e480274d
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-13 23:10:19 +0900

    bool.js

---
+++ Day_22/bool.js
@@ -0,0 +1,15 @@
+//아니 할게 있노!!
+//true or false 알겠지?
+
+//끝
+
+let isActiveGame = true;
+isActiveGame;
+//true
+
+let isGameOver = 1;
+
+let isLoggedIn = false;
+isLoggedIn = 372;
+isLoggedIn;
+//372
---
+++ Day_22/const_var.js
@@ -0,0 +1,20 @@
+//const = constant
+const luckyNum = 26;
+luckyNum += 1;
+luckyNum = 7;
+//둘다 안됨  const는 걍 id같은거라 생각하면 될 듯. 안바뀜
+const pi = 3.14159
+//const는 변수 조정 불가
+// 대신 const를 쓰면 js 업데이트를 막음.
+//바꿀수 있는 값은 let
+
+
+
+//--------------------------------------------------------
+//var
+//var는 옛날에 쓰던거임.
+var runDistance = 26.2;
+runDistance 
+//26.2
+//걍 var 쓸 이유 없음 걍 let이랑 const만 쓰면될듯
+//가끔 var도 쓰긴한다네영
