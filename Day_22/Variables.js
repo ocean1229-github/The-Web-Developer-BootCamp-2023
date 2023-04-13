b3fb67f7431121c8b43a1d753fcac0753ae37e5c
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-13 22:42:54 +0900

    Variables.js

---
+++ Day_22/Variables.js
@@ -0,0 +1,15 @@
+let numHens = 5;
+let numRoosters = 1;
+numHens + numRoosters
+// 6 -> 당연하긋지,,줠라 쉬운데?
+// 응 ~ 첫날이라 그래~
+let totalChickens = numHes + numRoosters;
+totalChickens
+//6
+// 간만에 언어 공부하는거 같노
+
+numHens = numHens + 1;
+// 6
+
+// totalChickens 는 아직 6으로 그대로
+// 이거 바꿔주려면 다시 설정해줘야함
