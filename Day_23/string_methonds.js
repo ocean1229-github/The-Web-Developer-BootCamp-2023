96ca1bdf2104ddbd0572d38395fc9b884135d838
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-14 22:38:08 +0900

    string_methods

---
+++ Day_23/string_methonds.js
@@ -0,0 +1,18 @@
+let msg = "leave me alone right now i hate you plz";
+msg.toUpperCase();
+//위에 문장 전체 대문자로 바꿔버리기
+let angryMsg = Msg.toUpperCase()
+angryMsg;
+"LOLOLOLOL!!!!!".toLowerCase()
+//하면 전체 소문자
+
+let greeting = '            leave me alone       '
+//trim은 양끝 공백을 지워준다
+greeting.trim()
+//개인정보 같은거 받을 때 유용하게 쓰인다고 함.
+
+greeting.trim()
+//'leave me alone'
+greeting.trim().toUpperCase()
+//'LEAVE ME ALONE'
+
