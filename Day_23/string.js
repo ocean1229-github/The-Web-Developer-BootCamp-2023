f255709ab50a006867e2ffdf0a5c14a6ffaf2924
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-14 22:16:21 +0900

    string.js

---
+++ Day_23/string.js
@@ -0,0 +1,11 @@
+let username = "danny";
+username
+//"danny"
+let favANIMAL = 'dUMBO oCTOPUS';
+// 이렇게해도되는데ㅐ " 문자열~ ' 이건 안돼~ 
+
+""
+//""이것도 하나의 문자열이다.
+
+// 작은놈이랑 큰따옴표랑 섞을때는 이럴때만.
+"I told her 'go away'"
