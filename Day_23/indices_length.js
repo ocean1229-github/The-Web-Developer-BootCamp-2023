5fe255139f612c3f18a19c68cdc9e66b53471d73
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-14 22:27:09 +0900

    indices_length.js

---
+++ Day_23/indices_length.js
@@ -0,0 +1,33 @@
+//O C T O P U S
+//1 2 3 4 5 6 7
+//위에가 문자열 인덱스 
+
+//D U M B O   O C T O  P  U  S
+//1 2 3 4 5 6 7 8 9 10 11 12 13
+//위에가 문자열 인덱스 
+
+animal = "Dumbo Octopus";
+animal.length;
+//13
+//이걸 알면 보통 첫번째나 마지막 문자 찾는데 쓰임
+
+"lol" + "lol";
+// "lololl"
+
+let firstName = "River";
+let lastName = "Phoenix";
+firstName + lastName;
+//RiverPhoenix
+
+let fullName = firstName + " " + lastName;
+//이건 가능~
+fullName;
+//"River Phoenix"
+
+1 + "h1"
+//"1hi"
+let result = 1 + "hi"
+result
+"1hi"
+typeof result
+//"string"
