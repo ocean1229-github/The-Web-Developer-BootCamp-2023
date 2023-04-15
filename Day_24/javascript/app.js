758da4867dfac2baf474c0bb20edeb5c1531793d
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-15 23:30:12 +0900

    if,else if, else.js

--- Day_24/javascript/app.js
+++ Day_24/javascript/app.js
@@ -10,8 +10,32 @@ if(random < 0.5){
 }
 
 if(random > 0.5){
-    console.log("YOUR NUMBER IS Great THAN 0.5");
+    console.log("YOUR NUMBER IS Greater THAN 0.5");
     console.log(random);
 }
 console.log("AFTER CONDITIONAL")
 
+
+const dayOfWeek = prompt('Enter a day').toLowerCase();
+
+if(dayOfWeek === 'monday'){
+    console.log("UGHHH I HATE MONDAYS!");
+} else if(dayOfWeek === 'saturday'){
+    console.log("Yay I Love Saturdays!");
+} else if(dayOfWeek === 'friday'){
+    console.log("Friday is good");
+}else{
+    console.log("tired..");
+}
+
+const age = 8;
+
+if(age < 5){
+    console.log("U are a baby. U get in for free!");
+} else if(age < 10){
+    console.log("U are a Child. You pay $10")
+} else if(age < 65){
+    console.log("U are a adult. You pay $20")
+} else{
+    console.log("U are a Senior. You pay $10")
+}
