f7097ed461730cbaca747fe8cc276dfb6e3f0662
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-15 22:33:15 +0900

    20_String Template LiteralExercise

---
+++ Coding Practice/20_String Template LiteralExercise.js
@@ -0,0 +1,7 @@
+// NO TOUCHING! (please)
+const die1 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+const die2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+
+// YOUR CODE BELOW THIS LINE:
+let sumdie = die1 + die2;
+let roll = 'roll:"You rolled a ' + die1 + 'and a ' + die2 + '. They sum to ' + sumdie + '"'; 
