deb0eff163fcce6fb764d8903707492d7f764245
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-15 23:07:24 +0900

    if.js

--- Day_24/javascript/app.js
+++ Day_24/javascript/app.js
@@ -1,1 +1,17 @@
 console.log("HELLO FROM OUR FIRST JS FILE!!!");
+
+
+
+console.log("BEFORE CONDITIONAL")
+let random = Math.random();
+if(random < 0.5){
+    console.log("YOUR NUMBER IS LESS THAN 0.5");
+    console.log(random);
+}
+
+if(random > 0.5){
+    console.log("YOUR NUMBER IS Great THAN 0.5");
+    console.log(random);
+}
+console.log("AFTER CONDITIONAL")
+
