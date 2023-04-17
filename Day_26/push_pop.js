91709a563483fa395b96e2f85b15235671a654f7
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-17 22:15:35 +0900

    push_pop.js

---
+++ Day_26/push_pop.js
@@ -0,0 +1,31 @@
+//push - add to end
+//pop - remove from end
+let movieLine = ['tom', 'nancy'];
+movieLine[2] = 'pablo';
+movieLine.push('oliver');
+
+let cat = "blue";
+cat.toUpperCase();
+movieLine.push('eva');
+movieLine.push('harry', 'hji');
+
+// pop은 삭제 따라서 pop에는 인수가 안들어감.
+movieLine.pop();
+movieLine;
+let person=movieLine.pop();
+
+person
+
+
+
+//--------------------------------------------------
+let barbell = [];
+barbell.push(45);
+barbell.push(45);
+barbell.push(25);
+barbell.push(10);
+barbell.push(2.5);
+barbell.pop();
+barbell.pop();
+barbell.pop();
+barbell.pop();
