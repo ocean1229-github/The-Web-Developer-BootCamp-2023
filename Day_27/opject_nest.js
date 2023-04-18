baee20d39e4408b4672ede5a154292b028835a9b
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-18 23:07:38 +0900

    object 배열과 객체를 골고루 사용하는 것에 익숙해지기.

---
+++ Day_27/opject_nest.js
@@ -0,0 +1,18 @@
+const commets = [
+    {
+        username:'Tammy',
+        text: 'lolololol',
+        votes: 9
+    },
+    {
+        username:'FishBoi',
+        text: 'glub glub',
+        votes: 12387
+    }
+];
+
+commets[1];
+//FishBoi 정보나옴.
+
+
+// 배열과 객체를 골고루 사용한다는 것을 기억하기.
