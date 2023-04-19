aebe3b30a04ee6b5ad9f9305b6e2d3a71469c3ad
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-19 22:22:15 +0900

    loop 예제 조금만 더 살펴보기

--- Day_28/loops.js
+++ Day_28/loops.js
@@ -7,6 +7,27 @@
 // console.log(7)
 // console.log(8)
 
-for(let num = 1; num<= 10; num++){
+// for(let num = 1; num<= 10; num++){
+//     console.log(num);
+// }
+
+
+// for(let num = 0; num<= 20; num+=2){
+//     console.log(num);
+//     //짝수 구하기
+// }
+
+// for(let num = 1; num<= 20; num+=2){
+//     console.log(num);
+//     //홀수 구하기
+// }
+
+// for(let hum = 100; hum >= 0; hum -= 20){
+//     console.log(hum);
+// }
+
+for(let num = 1; num<= 1000; num*=10){
     console.log(num);
+    //10 100 1000
 }
+
