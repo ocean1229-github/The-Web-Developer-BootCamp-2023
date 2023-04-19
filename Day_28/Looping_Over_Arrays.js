060a4c44929cb63b17bc6b25d850178eaa0d1889
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-19 22:50:42 +0900

    Looping_Over_Arrays.js

---
+++ Day_28/Looping_Over_Arrays.js
@@ -0,0 +1,10 @@
+const animals = ['lions', 'tigers', 'bears'];
+
+for(let i = 0; i < animals.length; i++){
+    console.log(animals[i]);
+}
+
+for(let i = animals.length; i>=0; i--){
+    console.log(animals[i]);
+}
+
