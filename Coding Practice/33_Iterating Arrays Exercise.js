57213a4c50660b14cd1509da7340eedcfe4b0434
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-19 22:51:34 +0900

    33_Iterating Arrays Exercise

---
+++ Coding Practice/33_Iterating Arrays Exercise.js
@@ -0,0 +1,8 @@
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+for(let i = 0; i < people.length;  i++){
+    console.log(people[i].toUpperCase());
+}
+
+// uppercase 함수 잘기억하자,,
