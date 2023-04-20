651c89cdb94ac24490a5353f50791f915286a67c
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-20 22:53:17 +0900

    For of.js

---
+++ Day_29/ForLoopUse.js
@@ -0,0 +1,18 @@
+const subscribe = ['a', 'b', 'c'];
+
+for(let i = 0; i < subscribe.length; i ++){
+    console.log('Visit reddit.com/r/${subrcribe[i]}')
+}
+
+for( let sub of subscribe){
+    console.log(sub)
+}
+
+for(let char of "hello world"){
+    console.log(char)
+    //각 단어 순서대로 나온다.
+    //h
+    //e
+    //l
+    //~~~
+}
