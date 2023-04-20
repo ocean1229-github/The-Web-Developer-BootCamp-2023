3deb155eaad1b4039e8f9c08892ae41d5bd2faaa
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-20 22:53:30 +0900

    34_For...Of Practice

---
+++ Coding Practice/34_ For...Of Practice.js
@@ -0,0 +1,9 @@
+const numbers = [1,2,3,4,5,6,7,8,9]; //DON'T CHANGE THIS LINE PLEASE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+let me = 1;
+for(let nums of numbers){
+    let answer = nums * me;
+    me ++;
+    console.log(answer);
+}
