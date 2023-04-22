5c1eab70a2c7e6a294a1a46f5e0b3d88ce945ceb
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-22 22:57:25 +0900

    37_Multiple Args Exercise

---
+++ Coding Practice/37_Multiple Args Exercise.js
@@ -0,0 +1,7 @@
+function isSnakeEyes(firstNum, secondNum){
+    if(firstNum === secondNum && firstNum == 1){
+        console.log("Snake Eyes!");
+    }else{
+        console.log("Not Snake Eyes!");
+    }
+}
