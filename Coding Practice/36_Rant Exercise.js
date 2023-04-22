49581dcdbfc9d3093dd9ad7af0fea248b4e6ddd7
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-22 22:45:40 +0900

    36_Rant Exercise

---
+++ Coding Practice/36_Rant Exercise.js
@@ -0,0 +1,8 @@
+// DEFINE YOUR FUNCTION:
+function rant(message){
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+    console.log(message.toUpperCase());
+}
+
+rant("I hate beets");
