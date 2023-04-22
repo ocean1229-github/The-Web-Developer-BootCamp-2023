79dd07eeaea7f12b78f7651353b8476e93143896
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-22 22:15:15 +0900

    function.js

---
+++ Day_31/function.js
@@ -0,0 +1,14 @@
+function singSong(){
+    console.log("DO");
+    console.log("RE");
+    console.log("MI");
+}
+
+singSong();
+singSong();
+singSong();
+singSong();
+singSong();
+singSong();
+singSong();
+singSong();
