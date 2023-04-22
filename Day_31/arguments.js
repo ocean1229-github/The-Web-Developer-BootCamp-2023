3f401a39fde5005bb0b59207deb463b10e244b27
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-04-22 22:52:22 +0900

    두개 이상의 인수의 경우

--- Day_31/arguments.js
+++ Day_31/arguments.js
@@ -2,3 +2,15 @@ function gree(idiotPerson){
     a.toUpperCase() ;
     console.log('Hey there, ${firstName}!')
 }
+
+function greet(firstName, lastName){
+    console.log(('Hey there, ${firstName} ${lastName[0]}!'))
+}
+
+function repeat(str, num){
+    let result = '';
+    for(let i = 0; i < str; i++){
+        result += str;
+    }
+    console.log(result)
+}
