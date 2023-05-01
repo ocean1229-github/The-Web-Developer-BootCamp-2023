274a37d2f8bb74fef7bbf62125a7fdf9bedb24dc
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-05-01 22:41:58 +0900

    return function.js

--- Day_31/HigherOtherFunction.js
+++ Day_31/HigherOtherFunction.js
@@ -18,3 +18,59 @@ function callTenTimes(f){
     }
 }
 
+const mystery = function makeMysteryFunc(){
+    const rand = Math.random()
+    if(rand > 0.5){
+        return function(){
+            console.log("CONGRATS, I AM A GOOD FUNCTION!")
+            console.log("YOU WIN~")
+        }
+     } else {
+            return function(){
+                alert("YOU HAVE BEEN INFECTED BY A COMPUTER VIURS");
+                alert("STOP TRYING TO CLOSE THIS WINDOW!");
+                alert("STOP TRYING TO CLOSE THIS WINDOW!");
+                alert("STOP TRYING TO CLOSE THIS WINDOW!");
+                alert("STOP TRYING TO CLOSE THIS WINDOW!");
+                alert("STOP TRYING TO CLOSE THIS WINDOW!");
+                alert("STOP TRYING TO CLOSE THIS WINDOW!");
+            }
+        }
+}
+
+//mystery만 누르면 이 코드가 실행된다.
+
+
+function makeBetweenFunc(min, max){
+    return function(num){
+        return num >= min && num <= max;
+    }
+}
+const testRange = function(num){
+    return num >= 100 && num <= 200
+}
+
+// makeBetweenFunc(50, 100) =>
+
+function isBetween(num){
+    return num >= 50 && num <= 100;
+}
+function isBetween2(num){
+    return num >= 1 && num <= 10;
+}
+
+//factory function 
+
+//--------------------------------------------------------
+//const isChild = makeBetweenFunc(0, 18)
+//이렇게하면 isChild라는 함수에 저 조건함수가 들어간다 이후
+//isChild(40)
+//false
+//가 뜨는 식이다 
+
+const isAdult = makeBetweenFunc(19, 64);
+const isSenior = makeBetweenFunc(65, 120);
+//isSenior(40)
+//false
+//isSenior(90)
+//true
