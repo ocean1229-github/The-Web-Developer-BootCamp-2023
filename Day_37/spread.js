92f4376c181b1fff96e2546430a4c56f4ed38657
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-05-04 22:49:47 +0900

    spread.js

---
+++ Day_37/spread.js
@@ -0,0 +1,17 @@
+Math.max(13,4,5,221,612,6,23,6273,333)
+Math.min(2,5,1)
+Math.max(nums)
+//NaN
+const nums = [1,2,3,4,,5,61,632,6,2,36,326,21,36,213,612,36,12]
+
+Math.max(...nums)
+//max함수가 잘써진다
+
+Math.max(nums);
+//안됨
+
+console.log('hello')
+//hello
+console.log(...'hello')
+//h e l l o
+//이런식으로 전개가된다.
