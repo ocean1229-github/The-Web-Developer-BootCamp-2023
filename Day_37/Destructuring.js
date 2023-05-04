9ec96083f16cd5d5d3fdc4ed99e1f9418d829ba7
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-05-04 23:07:50 +0900

    Destructuring.js

---
+++ Day_37/Destructuring.js
@@ -0,0 +1,8 @@
+const scores = [312451234,2134132333, 2316123, 23161326, 1236316, 213233214,  61263125, 123234]
+
+const highScore = scores[0];
+const secondHighScore = scores[1];
+
+const [gold, silver, bronze, ...everyoneElse] = scores;
+//scores의 순서대로 gold, silber, bronze 가 입력된다.
+
