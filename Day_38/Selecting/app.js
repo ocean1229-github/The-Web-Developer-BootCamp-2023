69da0398bd2d1dd935c3689271002e1e19c110a2
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-05-05 22:42:11 +0900

    getElementsByTagName getElementsByName 을 배웠긴한데 이 둘의 차이점에 대해 다시 한 번 공부해보는 것도 좋을 듯 하다.

--- Day_38/Selecting/app.js
+++ Day_38/Selecting/app.js
@@ -6,3 +6,23 @@ document.getElementById('toc')
 const toc = document.getElementById('toc')
 //이 객체의 특성들을 다 보여준다.
 //div#toc
+
+//---------------------------------
+
+document.getElementByTagName('img')
+//HTMLCollection(4)
+
+const allImages = document.getElementsByTagName('img')
+
+for(let img of allImages){
+    console.log(img.src)
+}
+//html에 있는 이미지의 출처 모두를 다 보여준다(4개)
+//배열이 아니다.
+
+const squareImages = document.getElementsByTagName('square');
+
+// for(let img of squareImages){
+//     img.src = '사진 중 한개의 출처를 넣음'
+// }
+
