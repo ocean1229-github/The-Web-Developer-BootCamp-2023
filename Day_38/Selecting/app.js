ef1b240bd339dfb10e7263aef1ab222b8d300317
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-05-05 22:24:39 +0900

    getElementById.js

---
+++ Day_38/Selecting/app.js
@@ -0,0 +1,8 @@
+//id와 일치하는 내용을 객체에서 가져와라
+
+const banner = document.getElementById('banner')
+
+document.getElementById('toc')
+const toc = document.getElementById('toc')
+//이 객체의 특성들을 다 보여준다.
+//div#toc
