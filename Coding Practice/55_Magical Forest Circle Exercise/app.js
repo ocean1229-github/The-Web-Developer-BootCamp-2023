c9e1d0479746bec44dee00b127a682e58c8b3c18
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-05-07 23:51:20 +0900

    55_Magical Forest Circle Exercise

---
+++ Coding Practice/55_Magical Forest Circle Exercise/app.js
@@ -0,0 +1,3 @@
+document.querySelector('#container').style.textAlign = 'center';
+document.querySelector('div>img').style.width = '150px'
+document.querySelector('div>img').style.borderRadius = '50%'
