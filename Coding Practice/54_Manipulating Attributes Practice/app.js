ebeb733725a8b38f9b3ea95e13a88e7fdba05644
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-05-07 23:06:56 +0900

    54_Manipulating Attributes Practice

---
+++ Coding Practice/54_Manipulating Attributes Practice/app.js
@@ -0,0 +1,7 @@
+// The url you need: 'https://devsprouthosting.com/images/chicken.jpg'
+
+const image = document.querySelector('img');
+
+image.setAttribute('src', 'https://devsprouthosting.com/images/chicken.jpg')
+
+image.setAttribute('alt', 'chicken')
