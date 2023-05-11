97e5d8fdfcabc456b1efef1da9e2107f7246d9db
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-05-11 23:42:38 +0900

    58_100 Button Insanity Exercise

---
+++ Coding Practice/58_100 Button Insanity Exercise/app.js
@@ -0,0 +1,8 @@
+// WRITE YOUR CODE IN HERE:
+const container = document.querySelector('#container')
+
+for(let i=0; i<100; i++){
+    const button = document.createElement("button")
+    button.innerText = "Hey!"
+    container.appendChild(button)
+}
