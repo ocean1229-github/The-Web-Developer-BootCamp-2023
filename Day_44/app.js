a659f8353695f9e8cb5b7ffee7beeb59ff0ca03a
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-05-15 22:59:04 +0900

    addEventListener

--- Day_44/app.js
+++ Day_44/app.js
@@ -16,3 +16,34 @@ btn.onmouseenter = scream;
 document.querySelector('h1').onclick = () => {
     alert('you clicked the h1')
 }
+
+const btn3 = document.querySelector('#v3');
+btn3.addEventListener('dblclick', function(){
+    alert('Clickede!')
+    //더블 클릭
+})
+// btn3.addEventListener('dblclick', scream)
+//     //더블 클릭
+// })
+
+// 위 방법은 별로 좋지 않다.
+
+// mouseup, mousedown
+
+function twist(){
+    console.log("TWIST")
+}
+function shout(){
+    console.log("SHOUT!")
+}
+
+const tasButton = document.querySelector('#tas')
+
+// tasButton.onclick = twist;
+// tasButton.onclick = shout;
+//onclick은 하나의 속성
+
+tasButton.addEventListener('click', twist, {once: true})
+tasButton.addEventListener('click', shout)
+// 이렇게 하면 twist shout모두가 적용이 된다.
+// once의 경우 한번 실행되면 그 뒤로는 실행되지 않는다.
