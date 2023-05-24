9d27a194decc51b5c79552ca16b17b56bec5fa41
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-05-24 22:33:12 +0900

    Score Kipper Project 2

--- Day_48~Day_49/app.js
+++ Day_48~Day_49/app.js
@@ -1,7 +1,9 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#reset')
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
@@ -13,6 +15,8 @@ p1Button.addEventListener('click', function(){
         p1Score += 1;
         if(p1Score === winningScore){
             isGameOver = true;
+            p1Display.classList.add('winner');
+            p2Display.classList.add('loser');
         }
         p1Display.textContent = p1Score;
 
@@ -23,8 +27,26 @@ p2Button.addEventListener('click', function(){
         p2Score += 1;
         if(p2Score === winningScore){
             isGameOver = true;
+            p2Display.classList.add('winner');
+            p1Display.classList.add('loser');
         }
         p2Display.textContent = p2Score;
 
     }
 })
+
+winningScoreSelect.addEventListener('change', function(){
+    winningScore = parseInt(this.value)
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset(){
+    isgameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.add('winner', 'loser');
+    p2Display.classList.add('winner', 'loser');
+}
