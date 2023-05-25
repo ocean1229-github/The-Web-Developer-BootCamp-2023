2938cda4f35302e1fcd3ea91b5e5feb7f0edc712
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-05-25 22:40:11 +0900

    Score Keeper Project Complete

---
+++ Day_48~Day_50/app.js
@@ -0,0 +1,62 @@
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+}
+
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
+}
+
+const resetButton = document.querySelector('#reset')
+const winningScoreSelect = document.querySelector('#playto');
+
+let winningScore = 3;
+let isGameOver = false;
+
+function updateScores(player, opponet){
+    if(!isgameOver){
+        player += 1;
+        if(p1Score === winningScore){
+            isGameOver = true;
+            player.display.classList.add('has-text-success');
+            opponet.display.classList.add('has-text-danger');
+            player.button.disabled= true;
+            opponet.button.disabled= true;
+        }
+        player.display.textContent = player.score;
+    }
+}
+
+p1Button.addEventListener('click', function(){
+    updateScores(p1, p2)
+})
+p2Button.addEventListener('click', function(){
+    updateScores(p2, p1)    
+})
+
+winningScoreSelect.addEventListener('change', function(){
+    winningScore = parseInt(this.value)
+})
+
+resetButton.addEventListener('click', reset)
+
+function reset(){
+    isgameOver = false;
+    for(let p of [p1, p2]){
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success', 'has-text-danger');
+        p.button.disabled = false;
+    }
+    // p1.score = 0;
+    // p2.score = 0;
+    // p1.display.textContent = 0;
+    // p2.display.textContent = 0;
+    // p1.display.classList.add('has-text-success', 'has-text-danger');
+    // p2.display.classList.add('has-text-success', 'has-text-danger');
+    // p1.button.disabled= false;
+    // p2.button.disabled= false;
+}
