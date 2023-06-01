c133ca30b7bbd809d07dcc6a2d1b2e7972a96640
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-06-01 22:15:19 +0900

    try, catch 비동기 함수 오류 처리하기

--- Day_55/Await/app.js
+++ Day_55/Await/app.js
@@ -42,7 +42,7 @@ const fakeRequestPromise = (url) => {
     return new Promise((resolve, reject) => {
         const delay = Math.floor(Math.random() * (4500)) + 500;
         setTimeout(() => {
-            if(delay > 4000){
+            if(delay > 2000){
                 reject('Connection Timeout :(')
             }else{
                 resolve(`Here is your fake data from ${url}`)
@@ -52,6 +52,14 @@ const fakeRequestPromise = (url) => {
 }
 
 async function makeTwoRequests(){
-    let data1 = await fakeRequestPromise('/page1');
-    console.log(data1)
+    try {
+        
+        let data1 = await fakeRequestPromise('/page1');
+        console.log(data1)
+        let data2 = await fakeRequestPromise('/page2');
+        console.log(data2)
+    } catch (e) {
+        cosnole.log("Caught an Error!");
+        console.log("error is:", e)
+    }
 }
