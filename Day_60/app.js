4dfc89a35498d2529ec1c5bb403027e65b5db4d5
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-06-07 22:35:03 +0900

    Axios를 비동기함수로 작성하기

--- Day_60/app.js
+++ Day_60/app.js
@@ -1,6 +1,21 @@
-axios.get("https://swapi.dev/api/people/1/").then(res => {
-    console.log("RESPONSE: ", res);
-})
-.catch(e => {
-    console.log("ERROR!",e);
-})
+// axios.get("https://swapi.dev/api/people/1/").then(res => {
+//     console.log("RESPONSE: ", res);
+// })
+// .catch(e => {
+//     console.log("ERROR!",e);
+// })
+
+//비동기함수로
+
+const getStarwarsPerson = async (id) => {
+    try{
+
+        const res = await axios.get(`https://swapi.dev/api/people/${id}/`);
+        console.log(res.data);
+    }catch(e){
+        console.log("ERROR", e);
+    }
+}
+
+getStarwarsPerson(5);
+getStarwarsPerson(10);
