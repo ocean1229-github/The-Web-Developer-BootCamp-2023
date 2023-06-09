787422e14c53862f25fb02181ba8f54d6eace670
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-06-09 22:48:48 +0900

    여기까지 코드 수정

--- Day_62/app.js
+++ Day_62/app.js
@@ -3,8 +3,9 @@ form.addEventListener('submit', async function (e) {
     e.preventDefault();
     // console.dir(form)
     // console.log(form.element.query.value)
-    const searchTerm = form.element.query.value;
+    const searchTerm = form.elements.query.value;
     const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`)
+    makeImages(res.data)
 })
 
 const makeImages = (shows) => {
