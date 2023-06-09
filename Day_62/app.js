cb2eadef324bd5df58762f3b6f41b46d6ebeab54
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-06-09 22:44:56 +0900

    영화 포스터 나열 코드

--- Day_62/app.js
+++ Day_62/app.js
@@ -5,7 +5,17 @@ form.addEventListener('submit', async function (e) {
     // console.log(form.element.query.value)
     const searchTerm = form.element.query.value;
     const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`)
-    const img = document.createElement('IMG');
-    img.src = res.data[0].show.image.medium;
-    document.body.append(img)
 })
+
+const makeImages = (shows) => {
+    for(let result of shows){
+        // console.log(result)
+        // 확인 코드
+
+        if(result.show.image){   
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            document.body.append(img)
+        }
+    }
+}
