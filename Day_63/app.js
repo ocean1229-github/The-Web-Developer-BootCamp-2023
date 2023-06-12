a219095f70597180927abec20729dc14c61b606a
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-06-12 22:43:24 +0900

    프로토타입 개념잡기

---
+++ Day_63/app.js
@@ -0,0 +1,27 @@
+// proto
+[]
+// -> __proto__
+
+arr.sing = function() {console.log("LA LA LA LA!")}
+// 프로토타입을 참조하는 특성
+// 프로토타입은 많은 메서드를 가지고있다.
+// 
+const body = document.body
+console.dir(body)
+
+// body의 특성들이 나옴
+// 아래는 프로토타입이 있다.
+// __proto__: HTMLBodyElement 프로토타입 템플릿에 해당하는
+// 즉, 이  html body 템플릿에 해당하는 다양한 메서드와 특성이 있다.
+
+// 다른 종류의 객체에 대한 프로토타입인 하나의 객체가 있다.
+
+Array.prototype
+//모든 프로토타입이 다있다.
+
+String.prototype
+
+String.prototype.grumpus = () => alert("GO AWAY!!")
+
+const cat = "Blue"
+cat.grumpus()
