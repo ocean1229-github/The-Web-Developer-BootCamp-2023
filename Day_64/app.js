43eb7aa0154ecad2caad87b446ac1d3c8a76876c
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-06-13 22:38:18 +0900

    Test

---
+++ Day_64/app.js
@@ -0,0 +1,35 @@
+function hex(r,g,b){
+    return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+function rgb(r, g, b){
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+// hex(255,100,25);
+// "#ff6419"
+// "rgb(255,100,25)"
+
+// 팩토리함수
+function makeColor(r,g,b){
+    const color = {};
+    color.r = r;
+    color.g = g;
+    color.b = b;
+    color.rgb = function() {
+        const { r, g, b } = this;
+        return `rgb(${r}, ${g}, ${b})`;
+    };
+    color.hex = function() {
+        const { r, g, b } = this;
+        return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    }
+    return color;
+}
+
+// makeColor(35,255,255)
+//r: 35, g: 255, b: 255
+
+const firstColor = makeColor(35,255,150);
+firstColor.hex()
+[1,2,3].push()
