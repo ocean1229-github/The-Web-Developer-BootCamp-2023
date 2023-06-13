ac77a059ed7b93a3adcc0f2c52af7a32ee00baed
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2023-06-13 22:41:01 +0900

    err 수정

--- Day_64/app.js
+++ Day_64/app.js
@@ -23,7 +23,7 @@ function makeColor(r, g, b){
     color.hex = function() {
         const { r, g, b } = this;
         return (
-            '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+            '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
         );
     };
     return color;
